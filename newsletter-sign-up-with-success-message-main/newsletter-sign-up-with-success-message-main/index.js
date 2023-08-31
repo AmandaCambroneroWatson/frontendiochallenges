74cb6ade4b827ba1ced5250034fdc7781ca2a874
Amanda Cambronero Watson <amandacwatson88@gmail.com>
2023-08-31 15:21:57 +0200

    files added

---
+++ newsletter-sign-up-with-success-message-main/newsletter-sign-up-with-success-message-main/index.js
@@ -0,0 +1,60 @@
+const form = document.querySelector("#newsletter-form");
+const submittedEmail = document.querySelector("#submitted-email");
+const signup = document.querySelector("#signup");
+const success = document.querySelector("#success");
+const dismiss = document.querySelector("#dismiss");
+
+function updateSuccessMessage(email) {
+  // update the strong tag with email
+  submittedEmail.textContent = email;
+}
+
+
+//toggle the cards 
+function switchSections() {
+  signup.classList.toggle("hidden");
+  success.classList.toggle("hidden");
+}
+
+function isValidEntry(str) {
+  const reg = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+  return reg.test(str);
+}
+
+function showErrors() {
+  const errorMss = document.querySelector(".error-message");
+  const input = document.querySelector("#email");
+  errorMss.classList.remove("hidden");
+  input.classList.add("error");
+}
+
+
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = form.querySelector("#email");
+  const valid = isValidEntry(email.value);
+  if (!valid) {
+    return showErrors();
+  }
+  if (valid) {
+    updateSuccessMessage(email.value);
+    switchSections();
+  }
+});
+
+dismiss.addEventListener("click", () => {
+  switchSections();
+  updateSuccessMessage("");
+  clearInput();
+});
+
+
+function clearInput(){
+    let getValue= document.getElementById("email");
+    // console.log(getValue.value)
+      if (getValue.value !="") {
+          getValue.value = "";
+      }
+}
